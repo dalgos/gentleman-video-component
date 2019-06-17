c902c2f70da64d688878fdfca246b9de1f604a95
kwangs <kwangs@tmon.co.kr>
2019-06-17 15:40:48 +0900

    feat: Add custom hook

---
+++ src/context.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react'
+import { createContext, Dispatch, SetStateAction, Reducer, ReducerState, ReducerAction, useReducer, useContext } from 'react'
+
+export enum ACTION_TYPES {
+  'MAXIMIZED_VIDEO' = 'MAXIMIZED_VIDEO',
+  'MINIMIZED_VIDEO' = 'MINIMIZED_VIDEO',
+  'UPDATED_VOLUME' = 'UPDATED_VOLUME',
+  'MUTED_VOLUME' = 'MUTED_VOLUME',
+}
+
+export interface PlayerState {
+  isMuted: boolean;
+  isVideoMaximized: boolean;
+  onToggleVideoSize: (isMaximized?: boolean) => void;
+  volume: number;
+}
+
+interface BaseAction {
+  type: keyof typeof ACTION_TYPES;
+}
+
+interface VideoAction extends BaseAction {
+  
+}
+
+interface VolumeAction extends BaseAction {
+  volume: number;
+}
+
+type PlayerAction = VideoAction | VolumeAction
+
+type PlayerReducer = Reducer<PlayerState, PlayerAction>
+type PlayerContextType = [ReducerState<PlayerReducer>, Dispatch<ReducerAction<PlayerReducer>>]
+
+const PlayerContext = createContext<PlayerContextType>([{
+  isMuted: true,
+  isVideoMaximized: false,
+  onToggleVideoSize: () => {},
+  volume: 0,
+}, (state: any) => state])
+
+interface PlayerProviderProps {
+  children: React.ReactChildren | React.ElementType | React.ReactNode;
+  initialState: PlayerState;
+}
+
+const reducer = (state: PlayerState, action: PlayerAction) => {
+  console.log(action)
+  switch(action.type) {
+    case ACTION_TYPES.MAXIMIZED_VIDEO:
+    case ACTION_TYPES.MINIMIZED_VIDEO:
+      return {
+        ...state,
+        isVideoMaximized: !state.isVideoMaximized,
+      }
+    case ACTION_TYPES.UPDATED_VOLUME:
+      return {
+        ...state,
+        isMuted: false,
+        volume: (action as VolumeAction).volume,
+      }
+    case ACTION_TYPES.MUTED_VOLUME:
+      return {
+        ...state,
+        isMuted: !state.isMuted,
+      }
+    default:
+      return state
+  }
+}
+
+export const updateVolumeAction: (volume: number) => VolumeAction = (volume) => ({ type: ACTION_TYPES.UPDATED_VOLUME, volume })
+export const muteVolumeAction = () => ({ type: ACTION_TYPES.MUTED_VOLUME })
+
+export const PlayerProvider: React.FunctionComponent<PlayerProviderProps> = ({ children, initialState }) => {
+  return (
+    <PlayerContext.Provider value={useReducer<Reducer<PlayerState, PlayerAction>>(reducer, initialState)} >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
+
+export const usePlayerStateValue = () => useContext<PlayerContextType>(PlayerContext)
