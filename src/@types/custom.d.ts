f945660d970d615477f8e8f8f56f96af2e2be6aa
kwangs <kwangs@tmon.co.kr>
2019-06-28 11:49:51 +0900

    fix: ES5 최적화
    * id3 모듈 직접 작성
    * babel 추가
    * custom type 변경

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,6 +1,7 @@
-declare module 'hls.js/src/demux/id3' {
+declare module 'utils/id3' {
   const ID3: {
     getID3Frames: (id3Data: Uint8Array) => Array<{ key: string, info: string, data: string }>;
   }
   export default ID3
 }
+
